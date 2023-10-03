3dedebbab08bea804786ca16883c19ea9e7a74d2
Sina-Soroosh <sinasoroosh07@gmail.com>
2023-10-03 21:26:46 +0330

    Add products data

---
+++ src/data.js
@@ -0,0 +1,317 @@
+const data = {
+  products: {
+    cosmetics: [
+      {
+        id: 1,
+        title: "Perfume",
+        shortName: "soap",
+        cover:
+          "https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-16.jpg",
+        price: 5,
+        priceDiscount: 5,
+        desc: "A button-front skirt in cobalt blue (one of the colors of this year) with pointed black spring shoes and a simple t-shirt can give you a chic style at the same time.",
+        options: [
+          "Made of 100% viscose",
+          "Wash with a washing machine at a maximum temperature of 30 degrees Celsius, iron on low temperature, do not dry clean, do not use a dryer, do not use bleach.",
+          "The dress has a simple and monochromatic fabric, the maxi shirt and its length is up to the ankles. This is the dress. The sides of the shirt have two relatively long slits. Chest equal to 38 cm - Waist equal to 33 cm",
+        ],
+      },
+      {
+        id: 2,
+        title: "Lip stick",
+        shortName: "lipstick",
+        cover:
+          "https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-19.jpg",
+        price: 1.9,
+        priceDiscount: 1.9,
+        desc: "A button-front skirt in cobalt blue (one of the colors of this year) with pointed black spring shoes and a simple t-shirt can give you a chic style at the same time.",
+        options: [
+          "Made of 100% viscose",
+          "Wash with a washing machine at a maximum temperature of 30 degrees Celsius, iron on low temperature, do not dry clean, do not use a dryer, do not use bleach.",
+          "The dress has a simple and monochromatic fabric, the maxi shirt and its length is up to the ankles. This is the dress. The sides of the shirt have two relatively long slits. Chest equal to 38 cm - Waist equal to 33 cm",
+        ],
+      },
+      {
+        id: 3,
+        title: "Washing gel",
+        shortName: "washing-gel",
+        cover:
+          "https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-5.jpg",
+        price: 1.2,
+        priceDiscount: 0.9,
+        desc: "A button-front skirt in cobalt blue (one of the colors of this year) with pointed black spring shoes and a simple t-shirt can give you a chic style at the same time.",
+        options: [
+          "Made of 100% viscose",
+          "Wash with a washing machine at a maximum temperature of 30 degrees Celsius, iron on low temperature, do not dry clean, do not use a dryer, do not use bleach.",
+          "The dress has a simple and monochromatic fabric, the maxi shirt and its length is up to the ankles. This is the dress. The sides of the shirt have two relatively long slits. Chest equal to 38 cm - Waist equal to 33 cm",
+        ],
+      },
+      {
+        id: 4,
+        title: "mask",
+        shortName: "Mask",
+        cover:
+          "https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-13.jpg",
+        price: 1.2,
+        priceDiscount: 0.8,
+        desc: "A button-front skirt in cobalt blue (one of the colors of this year) with pointed black spring shoes and a simple t-shirt can give you a chic style at the same time.",
+        options: [
+          "Made of 100% viscose",
+          "Wash with a washing machine at a maximum temperature of 30 degrees Celsius, iron on low temperature, do not dry clean, do not use a dryer, do not use bleach.",
+          "The dress has a simple and monochromatic fabric, the maxi shirt and its length is up to the ankles. This is the dress. The sides of the shirt have two relatively long slits. Chest equal to 38 cm - Waist equal to 33 cm",
+        ],
+      },
+    ],
+    accessories: [
+      {
+        id: 5,
+        title: "Glasses",
+        shortName: "glasses",
+        cover:
+          "https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-1.jpg",
+        price: 9.9,
+        priceDiscount: 8,
+        desc: "A button-front skirt in cobalt blue (one of the colors of this year) with pointed black spring shoes and a simple t-shirt can give you a chic style at the same time.",
+        options: [
+          "Made of 100% viscose",
+          "Wash with a washing machine at a maximum temperature of 30 degrees Celsius, iron on low temperature, do not dry clean, do not use a dryer, do not use bleach.",
+          "The dress has a simple and monochromatic fabric, the maxi shirt and its length is up to the ankles. This is the dress. The sides of the shirt have two relatively long slits. Chest equal to 38 cm - Waist equal to 33 cm",
+        ],
+      },
+      {
+        id: 6,
+        title: "Sunglasses",
+        shortName: "sunglasses",
+        cover:
+          "https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-15.jpg",
+        price: 5.9,
+        priceDiscount: 5.9,
+        desc: "A button-front skirt in cobalt blue (one of the colors of this year) with pointed black spring shoes and a simple t-shirt can give you a chic style at the same time.",
+        options: [
+          "Made of 100% viscose",
+          "Wash with a washing machine at a maximum temperature of 30 degrees Celsius, iron on low temperature, do not dry clean, do not use a dryer, do not use bleach.",
+          "The dress has a simple and monochromatic fabric, the maxi shirt and its length is up to the ankles. This is the dress. The sides of the shirt have two relatively long slits. Chest equal to 38 cm - Waist equal to 33 cm",
+        ],
+      },
+    ],
+    clothing: [
+      {
+        id: 7,
+        title: "Pet",
+        shortName: "pet",
+        cover:
+          "https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-7.jpg",
+        price: 79.9,
+        priceDiscount: 60,
+        desc: "A button-front skirt in cobalt blue (one of the colors of this year) with pointed black spring shoes and a simple t-shirt can give you a chic style at the same time.",
+        options: [
+          "Made of 100% viscose",
+          "Wash with a washing machine at a maximum temperature of 30 degrees Celsius, iron on low temperature, do not dry clean, do not use a dryer, do not use bleach.",
+          "The dress has a simple and monochromatic fabric, the maxi shirt and its length is up to the ankles. This is the dress. The sides of the shirt have two relatively long slits. Chest equal to 38 cm - Waist equal to 33 cm",
+        ],
+      },
+      {
+        id: 8,
+        title: "T-shirt",
+        shortName: "t-shirt",
+        cover:
+          "https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-11.jpg",
+        price: 7.9,
+        priceDiscount: 7.9,
+        desc: "A button-front skirt in cobalt blue (one of the colors of this year) with pointed black spring shoes and a simple t-shirt can give you a chic style at the same time.",
+        options: [
+          "Made of 100% viscose",
+          "Wash with a washing machine at a maximum temperature of 30 degrees Celsius, iron on low temperature, do not dry clean, do not use a dryer, do not use bleach.",
+          "The dress has a simple and monochromatic fabric, the maxi shirt and its length is up to the ankles. This is the dress. The sides of the shirt have two relatively long slits. Chest equal to 38 cm - Waist equal to 33 cm",
+        ],
+      },
+      {
+        id: 9,
+        title: "Jacket",
+        shortName: "jacket",
+        cover:
+          "https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-12.jpg",
+        price: 29.9,
+        priceDiscount: 29.9,
+        desc: "A button-front skirt in cobalt blue (one of the colors of this year) with pointed black spring shoes and a simple t-shirt can give you a chic style at the same time.",
+        options: [
+          "Made of 100% viscose",
+          "Wash with a washing machine at a maximum temperature of 30 degrees Celsius, iron on low temperature, do not dry clean, do not use a dryer, do not use bleach.",
+          "The dress has a simple and monochromatic fabric, the maxi shirt and its length is up to the ankles. This is the dress. The sides of the shirt have two relatively long slits. Chest equal to 38 cm - Waist equal to 33 cm",
+        ],
+      },
+      {
+        id: 10,
+        title: "Sweater",
+        shortName: "sweater",
+        cover:
+          "https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-18.jpg",
+        price: 10,
+        priceDiscount: 8.9,
+        desc: "A button-front skirt in cobalt blue (one of the colors of this year) with pointed black spring shoes and a simple t-shirt can give you a chic style at the same time.",
+        options: [
+          "Made of 100% viscose",
+          "Wash with a washing machine at a maximum temperature of 30 degrees Celsius, iron on low temperature, do not dry clean, do not use a dryer, do not use bleach.",
+          "The dress has a simple and monochromatic fabric, the maxi shirt and its length is up to the ankles. This is the dress. The sides of the shirt have two relatively long slits. Chest equal to 38 cm - Waist equal to 33 cm",
+        ],
+      },
+      {
+        id: 11,
+        title: "Pants",
+        shortName: "pants",
+        cover:
+          "https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-3.jpg",
+        price: 6.5,
+        priceDiscount: 5.5,
+        desc: "A button-front skirt in cobalt blue (one of the colors of this year) with pointed black spring shoes and a simple t-shirt can give you a chic style at the same time.",
+        options: [
+          "Made of 100% viscose",
+          "Wash with a washing machine at a maximum temperature of 30 degrees Celsius, iron on low temperature, do not dry clean, do not use a dryer, do not use bleach.",
+          "The dress has a simple and monochromatic fabric, the maxi shirt and its length is up to the ankles. This is the dress. The sides of the shirt have two relatively long slits. Chest equal to 38 cm - Waist equal to 33 cm",
+        ],
+      },
+      {
+        id: 12,
+        title: "Swim suit",
+        shortName: "Swim-suit",
+        cover:
+          "https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-9.jpg",
+        price: 4.9,
+        priceDiscount: 4.9,
+        desc: "A button-front skirt in cobalt blue (one of the colors of this year) with pointed black spring shoes and a simple t-shirt can give you a chic style at the same time.",
+        options: [
+          "Made of 100% viscose",
+          "Wash with a washing machine at a maximum temperature of 30 degrees Celsius, iron on low temperature, do not dry clean, do not use a dryer, do not use bleach.",
+          "The dress has a simple and monochromatic fabric, the maxi shirt and its length is up to the ankles. This is the dress. The sides of the shirt have two relatively long slits. Chest equal to 38 cm - Waist equal to 33 cm",
+        ],
+      },
+      {
+        id: 13,
+        title: "Dress",
+        shortName: "dress",
+        cover:
+          "https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-10.jpg",
+        price: 13.9,
+        priceDiscount: 9.9,
+        desc: "A button-front skirt in cobalt blue (one of the colors of this year) with pointed black spring shoes and a simple t-shirt can give you a chic style at the same time.",
+        options: [
+          "Made of 100% viscose",
+          "Wash with a washing machine at a maximum temperature of 30 degrees Celsius, iron on low temperature, do not dry clean, do not use a dryer, do not use bleach.",
+          "The dress has a simple and monochromatic fabric, the maxi shirt and its length is up to the ankles. This is the dress. The sides of the shirt have two relatively long slits. Chest equal to 38 cm - Waist equal to 33 cm",
+        ],
+      },
+    ],
+    jewelry: [
+      {
+        id: 14,
+        title: "Watch",
+        shortName: "watch",
+        cover:
+          "https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-8.jpg",
+        price: 49.9,
+        priceDiscount: 49.9,
+        desc: "A button-front skirt in cobalt blue (one of the colors of this year) with pointed black spring shoes and a simple t-shirt can give you a chic style at the same time.",
+        options: [
+          "Made of 100% viscose",
+          "Wash with a washing machine at a maximum temperature of 30 degrees Celsius, iron on low temperature, do not dry clean, do not use a dryer, do not use bleach.",
+          "The dress has a simple and monochromatic fabric, the maxi shirt and its length is up to the ankles. This is the dress. The sides of the shirt have two relatively long slits. Chest equal to 38 cm - Waist equal to 33 cm",
+        ],
+      },
+      {
+        id: 15,
+        title: "Necklaces",
+        shortName: "necklaces",
+        cover:
+          "https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-6.jpg",
+        price: 19.9,
+        priceDiscount: 19.9,
+        desc: "A button-front skirt in cobalt blue (one of the colors of this year) with pointed black spring shoes and a simple t-shirt can give you a chic style at the same time.",
+        options: [
+          "Made of 100% viscose",
+          "Wash with a washing machine at a maximum temperature of 30 degrees Celsius, iron on low temperature, do not dry clean, do not use a dryer, do not use bleach.",
+          "The dress has a simple and monochromatic fabric, the maxi shirt and its length is up to the ankles. This is the dress. The sides of the shirt have two relatively long slits. Chest equal to 38 cm - Waist equal to 33 cm",
+        ],
+      },
+      {
+        id: 16,
+        title: "Gold necklaces",
+        shortName: "gold-necklaces",
+        cover:
+          "https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-20.jpg",
+        price: 89.9,
+        priceDiscount: 89.9,
+        desc: "A button-front skirt in cobalt blue (one of the colors of this year) with pointed black spring shoes and a simple t-shirt can give you a chic style at the same time.",
+        options: [
+          "Made of 100% viscose",
+          "Wash with a washing machine at a maximum temperature of 30 degrees Celsius, iron on low temperature, do not dry clean, do not use a dryer, do not use bleach.",
+          "The dress has a simple and monochromatic fabric, the maxi shirt and its length is up to the ankles. This is the dress. The sides of the shirt have two relatively long slits. Chest equal to 38 cm - Waist equal to 33 cm",
+        ],
+      },
+    ],
+    shoes: [
+      {
+        id: 17,
+        title: "Slippers",
+        shortName: "slippers",
+        cover:
+          "https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-17.jpg",
+        price: 30,
+        priceDiscount: 30,
+        desc: "A button-front skirt in cobalt blue (one of the colors of this year) with pointed black spring shoes and a simple t-shirt can give you a chic style at the same time.",
+        options: [
+          "Made of 100% viscose",
+          "Wash with a washing machine at a maximum temperature of 30 degrees Celsius, iron on low temperature, do not dry clean, do not use a dryer, do not use bleach.",
+          "The dress has a simple and monochromatic fabric, the maxi shirt and its length is up to the ankles. This is the dress. The sides of the shirt have two relatively long slits. Chest equal to 38 cm - Waist equal to 33 cm",
+        ],
+      },
+      {
+        id: 18,
+        title: "Shoes",
+        shortName: "shoes",
+        cover:
+          "https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-4.jpg",
+        price: 89,
+        priceDiscount: 89,
+        desc: "A button-front skirt in cobalt blue (one of the colors of this year) with pointed black spring shoes and a simple t-shirt can give you a chic style at the same time.",
+        options: [
+          "Made of 100% viscose",
+          "Wash with a washing machine at a maximum temperature of 30 degrees Celsius, iron on low temperature, do not dry clean, do not use a dryer, do not use bleach.",
+          "The dress has a simple and monochromatic fabric, the maxi shirt and its length is up to the ankles. This is the dress. The sides of the shirt have two relatively long slits. Chest equal to 38 cm - Waist equal to 33 cm",
+        ],
+      },
+      {
+        id: 19,
+        title: "Formal shoes",
+        shortName: "formal-shoes",
+        cover:
+          "https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-14.jpg",
+        price: 19.9,
+        priceDiscount: 18.9,
+        desc: "A button-front skirt in cobalt blue (one of the colors of this year) with pointed black spring shoes and a simple t-shirt can give you a chic style at the same time.",
+        options: [
+          "Made of 100% viscose",
+          "Wash with a washing machine at a maximum temperature of 30 degrees Celsius, iron on low temperature, do not dry clean, do not use a dryer, do not use bleach.",
+          "The dress has a simple and monochromatic fabric, the maxi shirt and its length is up to the ankles. This is the dress. The sides of the shirt have two relatively long slits. Chest equal to 38 cm - Waist equal to 33 cm",
+        ],
+      },
+    ],
+    bags: [
+      {
+        id: 20,
+        title: "Leather bag",
+        shortName: "leather-bag",
+        cover:
+          "https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-2.jpg",
+        price: 15.6,
+        priceDiscount: 13,
+        desc: "A button-front skirt in cobalt blue (one of the colors of this year) with pointed black spring shoes and a simple t-shirt can give you a chic style at the same time.",
+        options: [
+          "Made of 100% viscose",
+          "Wash with a washing machine at a maximum temperature of 30 degrees Celsius, iron on low temperature, do not dry clean, do not use a dryer, do not use bleach.",
+          "The dress has a simple and monochromatic fabric, the maxi shirt and its length is up to the ankles. This is the dress. The sides of the shirt have two relatively long slits. Chest equal to 38 cm - Waist equal to 33 cm",
+        ],
+      },
+    ],
+  },
+  blogs: {},
+};
