d81b0ed2f5a9b96cde29ef30c2438a09945cf7ba
Sina-Soroosh <sinasoroosh07@gmail.com>
2023-10-03 14:11:36 +0330

    Add section offer in Index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { useRoutes } from "react-router-dom";
 import Header from "./components/Header/Header";
+import router from "./router";
 
 function App() {
+  let routes = useRoutes(router);
+
   return (
     <>
       <Header />
+      {routes}
     </>
   );
 }
---
+++ src/pages/Index/Index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Index.css";
+
+function Index() {
+  return (
+    <>
+      <section className="offer">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-6">
+              <h1>The best products for autumn</h1>
+              <p>
+                The autumn collection of the best brands in the world is at your
+                disposal with a discount and guarantee of authenticity
+              </p>
+              <a href="" className="special-products">
+                Special Products
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Index;
