9d30398bb28abd67ed6f0b79ccf6f3e0be1e9f9c
Sina-Soroosh <sinasoroosh07@gmail.com>
2023-10-03 11:12:38 +0330

    Expand header for size smaller than 768 pixels

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,52 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 
 export default function App() {
-  return (
-    <header>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-12">
-            <div className="content-header">
-              <div className="logo">
-                <img src="/images/logo/svg/logo-no-background.svg" alt="" />
-              </div>
+  const [isShowMenu, setIsShowMenu] = useState(false);
 
-              <div className="menu">
-                <ul>
-                  <li>
-                    <a href="">Home</a>
-                  </li>
-                  <li>
-                    <a href="">Products</a>
-                  </li>
-                  <li>
-                    <a href="">About us</a>
-                  </li>
-                  <li>
-                    <a href="">Blogs</a>
-                  </li>
-                  <li>
-                    <a href="">Contact us</a>
-                  </li>
-                  <li className="icon">
-                    <i className="fa-solid fa-user"></i>
-                  </li>
-                  <li className="icon">
-                    <i className="fa-solid fa-bag-shopping"></i>
-                    <div className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                      0
-                    </div>
-                  </li>
-                  <li className="icon">
-                    <i className="fa-solid fa-magnifying-glass"></i>
-                  </li>
-                </ul>
+  return (
+    <>
+      {isShowMenu ? (
+        <div id="cover-page" onClick={() => setIsShowMenu(false)}></div>
+      ) : null}
+      <header>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12">
+              <div className="content-header">
+                <div className="logo">
+                  <img src="/images/logo/svg/logo-no-background.svg" alt="" />
+                </div>
+
+                <div className="icon-menu" onClick={() => setIsShowMenu(true)}>
+                  <i className="fa-solid fa-bars"></i>
+                </div>
+
+                <div className={`menu ${isShowMenu ? "active" : ""}`}>
+                  <div
+                    className="close-menu"
+                    onClick={() => setIsShowMenu(false)}
+                  >
+                    <i className="fa-solid fa-xmark"></i>
+                  </div>
+                  <ul className="top-menu">
+                    <li className="icon">
+                      <i className="fa-solid fa-user"></i>
+                    </li>
+                    <li className="icon">
+                      <i className="fa-solid fa-bag-shopping"></i>
+                      <div className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                        0
+                      </div>
+                    </li>
+                    <li className="icon">
+                      <i className="fa-solid fa-magnifying-glass"></i>
+                    </li>
+                  </ul>
+                  <ul>
+                    <li>
+                      <a href="">Home</a>
+                    </li>
+                    <li>
+                      <a href="">Products</a>
+                    </li>
+                    <li>
+                      <a href="">About us</a>
+                    </li>
+                    <li>
+                      <a href="">Blogs</a>
+                    </li>
+                    <li>
+                      <a href="">Contact us</a>
+                    </li>
+                    <li className="icon">
+                      <i className="fa-solid fa-user"></i>
+                    </li>
+                    <li className="icon">
+                      <i className="fa-solid fa-bag-shopping"></i>
+                      <div className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                        0
+                      </div>
+                    </li>
+                    <li className="icon">
+                      <i className="fa-solid fa-magnifying-glass"></i>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 }
