cab5d2dcabb2cd1533d79799bc5ba01808fe4c76
Sina-Soroosh <sinasoroosh07@gmail.com>
2023-10-03 14:42:28 +0330

    Add section categories-images in Index.js

--- src/pages/Index/Index.js
+++ src/pages/Index/Index.js
@@ -20,6 +20,37 @@ function Index() {
           </div>
         </div>
       </section>
+
+      <section className="categories-images">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-4">
+              <a href="" className="category">
+                <img src="/images/index/store2-home-category-1.jpg" alt="" />
+                <div className="details-category">
+                  <h2>Clothing</h2>
+                </div>
+              </a>
+            </div>
+            <div className="col-md-4">
+              <a href="" className="category">
+                <img src="/images/index/store2-home-category-2-1.jpg" alt="" />
+                <div className="details-category">
+                  <h2>Jewellery</h2>
+                </div>
+              </a>
+            </div>
+            <div className="col-md-4">
+              <a href="" className="category">
+                <img src="/images/index/store2-home-category-3.jpg" alt="" />
+                <div className="details-category">
+                  <h2>Cosmetic</h2>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
