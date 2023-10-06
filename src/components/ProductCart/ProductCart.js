5b6f41c740567cf93488bcf578a703fdc348ff9a
Sina-Soroosh <sinasoroosh07@gmail.com>
2023-10-06 12:57:09 +0330

    Add logic Cart page

--- src/components/ProductCart/ProductCart.js
+++ src/components/ProductCart/ProductCart.js
@@ -1,32 +1,96 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./ProductCart.css";
+import { CartContext } from "../../context/CartContext";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
-function ProductCart() {
+function ProductCart(props) {
+  const [sumPrice, setSumPrice] = useState(0);
+  const { cart, setCart } = useContext(CartContext);
+  const swal = withReactContent(Swal);
+
+  useEffect(() => {
+    setSumPrice(props.priceDiscount * props.count);
+  }, [props.count]);
+
+  const changeCount = (count) => {
+    const newCart = [...cart];
+
+    if (count) {
+      let indexProduct = newCart.findIndex(
+        (product) => product.id === props.id
+      );
+
+      newCart[indexProduct].count = count;
+    } else {
+      deleteProduct();
+    }
+
+    setCart(newCart);
+  };
+
+  const deleteProduct = () => {
+    const copyCart = [...cart];
+
+    swal
+      .fire({
+        title: "Are you sure ?",
+        icon: "warning",
+        confirmButtonText: "Yes , remove product",
+        showCancelButton: true,
+        cancelButtonText: "No , I regret",
+      })
+      .then((res) => {
+        if (res.isConfirmed) {
+          let productDetails = copyCart.find(
+            (product) => product.id === props.id
+          );
+
+          let newCart = copyCart.filter(
+            (product) => product.id !== productDetails.id
+          );
+
+          swal.fire({
+            toast: true,
+            icon: "success",
+            title: "The product has been successfully removed to the cart",
+            position: "bottom-left",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener("mouseenter", swal.stopTimer);
+              toast.addEventListener("mouseleave", swal.resumeTimer);
+            },
+          });
+
+          setCart(newCart);
+        }
+      });
+  };
+
   return (
     <tr className="product-cart">
       <td className="image-product-cart">
-        <img
-          src="https://freedemo.ir/tbe/wp-content/uploads/2020/07/store2-product-19.jpg"
-          alt=""
-        />
+        <img src={props.cover} alt={props.title} />
       </td>
       <td>
-        <span>Lip stick</span>
+        <span>{props.title}</span>
       </td>
-      <td className="price-product-cart">20 $</td>
+      <td className="price-product-cart">{props.priceDiscount} $</td>
       <td>
         <div className="count-product-cart">
-          <span className="icon">
+          <span className="icon" onClick={() => changeCount(props.count - 1)}>
             <i className=" fa-solid fa-minus"></i>
           </span>
-          <span className="number">0</span>
-          <span className="icon">
+          <span className="number">{props.count}</span>
+          <span className="icon" onClick={() => changeCount(props.count + 1)}>
             <i className="fa-solid fa-plus"></i>
           </span>
         </div>
       </td>
-      <td className="price-product-cart">40 $</td>
-      <td className="delete-product">
+      <td className="price-product-cart">{sumPrice.toFixed(1)} $</td>
+      <td className="delete-product" onClick={deleteProduct}>
         <i className="fa-solid fa-xmark"></i>
       </td>
     </tr>
--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./Cart.css";
 import ProductCart from "../../components/ProductCart/ProductCart";
 import { Link } from "react-router-dom";
+import { CartContext } from "../../context/CartContext";
 
 function Cart() {
+  const [totalCart, setTotalCart] = useState(0);
+  const { cart, setCart } = useContext(CartContext);
+
+  useEffect(() => {
+    let sum = 0;
+
+    cart.forEach((product) => {
+      sum += product.price * product.count;
+    });
+
+    setTotalCart(sum);
+  }, [cart]);
+
+  const emptyCart = () => {
+    setCart([]);
+  };
+
   return (
     <div className="cart">
       <div className="container">
@@ -15,54 +33,65 @@ function Cart() {
           </div>
           <div className="col-12">
             <div className="content-cart">
-              <div className="row">
-                <div className="col-lg-8">
-                  <div className="table-products-cart">
-                    <table>
-                      <thead>
-                        <tr>
-                          <th></th>
-                          <th>Product</th>
-                          <th>Price</th>
-                          <th>Count</th>
-                          <th>Collection part</th>
-                          <th></th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <ProductCart />
-                        <ProductCart />
-                        <ProductCart />
-                      </tbody>
-                    </table>
-                  </div>
-                  <div className="btns-cart">
-                    <div className="empty-cart">
-                      <button>Empty Cart</button>
+              {cart.length ? (
+                <div className="row">
+                  <div className="col-lg-8">
+                    <div className="table-products-cart">
+                      <table>
+                        <thead>
+                          <tr>
+                            <th></th>
+                            <th>Product</th>
+                            <th>Price</th>
+                            <th>Count</th>
+                            <th>Collection part</th>
+                            <th></th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {cart.map((product) => (
+                            <ProductCart key={product.id} {...product} />
+                          ))}
+                        </tbody>
+                      </table>
                     </div>
-                    <div className="continue-cart">
-                      <Link to="/products">Continue shopping</Link>
+                    <div className="btns-cart">
+                      <div className="empty-cart">
+                        <button onClick={emptyCart}>Empty Cart</button>
+                      </div>
+                      <div className="continue-cart">
+                        <Link to="/products">Continue shopping</Link>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4">
+                    <div className="result-cart">
+                      <h3 className="title-result-cart">
+                        The total of the shopping cart
+                      </h3>
+                      <div className="total-result-cart">
+                        <span>Total</span>
+                        <span className="total">{totalCart.toFixed(1)} $</span>
+                      </div>
+                      <div className="checkout-btn">
+                        <Link to="/">Continue to settle the account</Link>
+                      </div>
+                      <div className="continue-shopping">
+                        <Link to="/products">Continue shopping</Link>
+                      </div>
                     </div>
                   </div>
                 </div>
-                <div className="col-lg-4">
-                  <div className="result-cart">
-                    <h3 className="title-result-cart">
-                      The total of the shopping cart
-                    </h3>
-                    <div className="total-result-cart">
-                      <span>Total</span>
-                      <span className="total">40 $</span>
-                    </div>
-                    <div className="checkout-btn">
-                      <Link to="/">Continue to settle the account</Link>
-                    </div>
-                    <div className="continue-shopping">
-                      <Link to="/products">Continue shopping</Link>
-                    </div>
+              ) : (
+                <>
+                  <div className="alert alert-danger text-center fs-5">
+                    Your shopping cart is currently empty.
                   </div>
-                </div>
-              </div>
+                  <div className="empty-link">
+                    <Link to="/products">Return to the store</Link>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </div>
